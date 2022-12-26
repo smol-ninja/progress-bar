7c544bb42c3451eab395ab9153f5e703c307de4d
Shubham Yadav <shubhamy2015@gmail.com>
2022-12-26 21:24:45 +0530

    🚀: an extensible progress bar similar to tqdm

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::{time::Duration, thread::sleep};
+
+const CLEAR: &str = "\x1B[2J\x1B[1;1H";
+
+struct Progress<Iter> {
+  iter: Iter,
+  i: usize
+}
+
+impl<Iter> Progress<Iter> {
+  pub fn new(iter: Iter) -> Self {
+    Progress {iter, i: 0}
+  }
+}
+
+impl<Iter> Iterator for Progress<Iter> 
+where Iter: Iterator {
+    type Item = Iter::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+      println!("{}{}", CLEAR, "*".repeat(self.i));
+      self.i += 1;
+
+      self.iter.next()
+    }
+}
+
+fn expensive_calculation(_n: &i32) {
+  sleep(Duration::from_secs(1));
+}
+
+fn main() {
+  let v = vec![1, 2, 3];
+  for n in Progress::new(v.iter()) {
+    expensive_calculation(n)
+  }
+}
